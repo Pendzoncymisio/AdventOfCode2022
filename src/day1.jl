875162e7a84ae6a7c5c2e3938d05645e8676e1c1
Pendzoncymisio <pendzoncymisio@gmail.com>
2022-12-01 23:24:51 +0100

    Refactor both parts to use sorted array

--- src/day1.jl
+++ src/day1.jl
@@ -2,25 +2,7 @@ using DelimitedFiles
 
 input_array = readdlm("$(@__DIR__)\\..\\inputs\\day1.txt",',',Any,skipblanks=false)
 
-function part1(input_array)
-    max_calories, current_calories = 0, 0
-
-    for row in input_array
-        if typeof(row) <: SubString{String}
-            if current_calories > max_calories
-                max_calories = current_calories
-            end
-            current_calories = 0
-        elseif typeof(row) <: Int64
-            current_calories += row
-        end
-    end
-    max_calories
-end
-
-#print("Part1: ",part1(input_array))
-
-function part2(input_array)
+function get_sorted_calories(input_array)
     current_calories = 0
     calories_array = []
 
@@ -33,7 +15,18 @@ function part2(input_array)
         end
     end
     sort!(calories_array,rev=true)
+end
+
+calories_array = get_sorted_calories(input_array)
+
+function part1(calories_array)
+    calories_array[1]
+end
+
+println("Part1: ",part1(calories_array))
+
+function part2(calories_array)
     calories_array[1] + calories_array[2] + calories_array[3]
 end
 
-print("Part2: ",part2(input_array))
+println("Part2: ",part2(calories_array))
