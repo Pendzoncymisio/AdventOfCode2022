bf794f66c7d0ccf9cbddaf10175d25213e72a2ee
Pendzoncymisio <pendzoncymisio@gmail.com>
2022-12-03 04:50:12 -0500

    Part2 but get_common_char bugged

--- src/day3.jl
+++ src/day3.jl
@@ -3,8 +3,14 @@ using DelimitedFiles
 input_array = readdlm(joinpath(@__DIR__,"..","inputs","day3.txt"),' ',String,skipblanks=false)
 
 function get_char_index(char::Char)
+    char == '_' && error("'_' passed to get_char_index")
     Int(char) > 93 && return Int(char) - 96
     return Int(char) - 38
+end
+
+function get_char_from_index(index::Int)
+    index <= 26 && return Char(index+96)
+    return Char(index+38)
 end
 
 function get_common_char(strings::Vector{String})
@@ -15,8 +21,9 @@ function get_common_char(strings::Vector{String})
             char_flags[get_char_index(char)] += 1
         end
     end
-
-    return indexin(length(strings),char_flags)
+    found_index = indexin(length(strings),char_flags)[1]
+    typeof(found_index) == Nothing && return '_'
+    return get_char_from_index(found_index)
 end
 get_common_char(values...) = get_common_char([values...])
 
@@ -27,10 +34,26 @@ function part1(input_array)
         value = row[1]
         val1 = value[1:Int(length(value)/2)]
         val2 = value[Int(length(value)/2)+1:end]
-        #println(value,' ',val1,' ',val2)
         points += get_char_index(get_common_char(val1,val2))
     end
     points
 end
 
-println("Part1: ",part1(input_array))
+#println("Part1: ",part1(input_array))
+
+function part2(input_array)
+    
+    points = 0
+    for (index,_) in enumerate(input_array[1:3:end])
+        common_char = get_common_char(input_array[index],input_array[index+1],input_array[index+2])
+        println("")
+        println(input_array[index]," ",input_array[index+1]," ",input_array[index+2])
+        println(common_char)
+        points += get_char_index(common_char)
+        
+    end
+    points
+end
+
+#println("Part2: ",part2(input_array))
+get_common_char("DD","DUPA")
