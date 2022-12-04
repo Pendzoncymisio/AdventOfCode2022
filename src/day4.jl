989ab1b85bbe3e94ec339efb9e87fa61accdb15d
Pendzoncymisio <pendzoncymisio@gmail.com>
2022-12-04 02:51:57 -0500

    Part2 done

--- src/day4.jl
+++ src/day4.jl
@@ -5,8 +5,8 @@ input_array = readdlm(joinpath(@__DIR__,"..","inputs","day4.txt"),',',String,ski
 
 function parse_range(input::String)
     val_min, val_max = parse.(Int,split(input,'-'))
-    #return val_min:val_max
-    return [val_min;val_max]
+    return val_min:val_max
+    #return [val_min;val_max] TODO: Clean this so it woud work for both parts
 end
 
 parsed_input_array = parse_range.(input_array)
@@ -21,4 +21,14 @@ function part1(parsed_input_array)
     points
 end
 
-println("Part1: ",part1(parsed_input_array))
+#println("Part1: ",part1(parsed_input_array))
+
+function part2(parsed_input_array)
+    points = 0
+    for row in eachrow(parsed_input_array)
+        length(intersect(row[1],row[2])) > 0 && (points += 1) 
+    end
+    points
+end
+
+println("Part2: ",part2(parsed_input_array))
