b8c85e322d444c2c5f50d05ac2ffefb8fcb74000
Pendzoncymisio <pendzoncymisio@gmail.com>
2022-12-04 12:34:25 +0100

    Day4 both parts working on ranges

--- src/day4.jl
+++ src/day4.jl
@@ -6,22 +6,22 @@ input_array = readdlm(joinpath(@__DIR__,"..","inputs","day4.txt"),',',String,ski
 function parse_range(input::String)
     val_min, val_max = parse.(Int,split(input,'-'))
     return val_min:val_max
-    #return [val_min;val_max] TODO: Clean this so it woud work for both parts
 end
 
 parsed_input_array = parse_range.(input_array)
 function part1(parsed_input_array)
     points = 0
+    points_debug = 0
     for row in eachrow(parsed_input_array)
-        if (row[1][1] <= row[2][1] && row[1][2] >= row[2][2]) || 
-            (row[1][1] >= row[2][1] && row[1][2] <= row[2][2])
+        if (row[1].start <= row[2].start && row[1].stop >= row[2].stop) || 
+            (row[1].start >= row[2].start && row[1].stop <= row[2].stop)
             points+=1
         end
     end
     points
 end
 
-#println("Part1: ",part1(parsed_input_array))
+println("Part1: ",part1(parsed_input_array))
 
 function part2(parsed_input_array)
     points = 0
