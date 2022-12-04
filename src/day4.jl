bb120836d074524b40ef9f08b1060d278011fba7
Pendzoncymisio <pendzoncymisio@gmail.com>
2022-12-04 02:49:36 -0500

    Part1 done

--- src/day4.jl
+++ src/day4.jl
@@ -1,3 +1,24 @@
 using DelimitedFiles
 
 input_array = readdlm(joinpath(@__DIR__,"..","inputs","day4.txt"),',',String,skipblanks=false)
+
+
+function parse_range(input::String)
+    val_min, val_max = parse.(Int,split(input,'-'))
+    #return val_min:val_max
+    return [val_min;val_max]
+end
+
+parsed_input_array = parse_range.(input_array)
+function part1(parsed_input_array)
+    points = 0
+    for row in eachrow(parsed_input_array)
+        if (row[1][1] <= row[2][1] && row[1][2] >= row[2][2]) || 
+            (row[1][1] >= row[2][1] && row[1][2] <= row[2][2])
+            points+=1
+        end
+    end
+    points
+end
+
+println("Part1: ",part1(parsed_input_array))
