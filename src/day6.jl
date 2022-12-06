0ec22f29fad0e270aeb9577b8a12711252b9c968
Pendzoncymisio <pendzoncymisio@gmail.com>
2022-12-06 08:48:10 +0100

    Part1 naively done

--- src/day6.jl
+++ src/day6.jl
@@ -1,4 +1,19 @@
 using DelimitedFiles
+using DataStructures
 
 input_array = readdlm(joinpath(@__DIR__,"..","inputs","day6.txt"),';',String,skipblanks=false)
 input_string = input_array[1]
+
+function part1(input_string)
+    letter_buffer = CircularBuffer{Char}(4)
+    for (index,input_letter) in enumerate(input_string)
+        push!(letter_buffer,input_letter)
+        if isfull(letter_buffer)
+            if letter_buffer[1] != letter_buffer[2] && letter_buffer[1] != letter_buffer[3] && letter_buffer[1] != letter_buffer[4] && letter_buffer[2] != letter_buffer[3] && letter_buffer[2] != letter_buffer[4] && letter_buffer[3] != letter_buffer[4]
+                return index
+            end
+        end
+    end
+end
+
+println("Part1: ",part1(input_string))
