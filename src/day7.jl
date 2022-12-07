5ec247ec11798a0c2638d0e23e83ca779aa8f227
Pendzoncymisio <pendzoncymisio@gmail.com>
2022-12-07 22:22:04 +0100

    Day7 done

--- src/day7.jl
+++ src/day7.jl
@@ -2,16 +2,65 @@ using DelimitedFiles
 using AbstractTrees
 
 input_array = readdlm(joinpath(@__DIR__,"..","inputs","day7.txt"))
-print(typeof(input_array[1]))
 
-for row in eachrow(input_array)
-    if row[1] == "\$"
-        println(row[3])
+function part1(input_array)
+
+    current_size = 0
+    result_size = 0
+    parents_tmp_array = []
+    for row in eachrow(input_array)
+        if row[1] == "\$"
+            if row[2] == "cd"
+                if row[3] == ".."
+                    parent_weight = pop!(parents_tmp_array)
+                    current_size < 100000 && (result_size += current_size)
+                    current_size += parent_weight
+                else
+                    push!(parents_tmp_array,current_size)
+                    current_size = 0
+                end
+            end
+        elseif typeof(row[1][1]) <: Int64
+            current_size += row[1]
+        end
     end
+    result_size
 end
 
-struct Directory
-    value::Int
-    children::Vector{Directory}
-enD
+println("Part1: ",part1(input_array))
 
+function get_space_to_free(input_array)
+    current_size = 0
+    for row in eachrow(input_array)
+        if typeof(row[1][1]) <: Int64
+            current_size += row[1]
+        end
+    end
+    30000000 - 70000000 + current_size
+end
+
+function part2(input_array)
+    space_to_free = get_space_to_free(input_array)
+    current_size = 0
+    parents_tmp_array = []
+    potential_dir = []
+    for row in eachrow(input_array)
+        if row[1] == "\$"
+            if row[2] == "cd"
+                if row[3] == ".."
+                    parent_weight = pop!(parents_tmp_array)
+                    current_size > space_to_free && (push!(potential_dir,current_size))
+                    current_size += parent_weight
+                else
+                    push!(parents_tmp_array,current_size)
+                    current_size = 0
+                end
+            end
+        elseif typeof(row[1][1]) <: Int64
+            current_size += row[1]
+        end
+    end
+    return minimum(potential_dir)
+end
+
+println("Part2: ",part2(input_array))
