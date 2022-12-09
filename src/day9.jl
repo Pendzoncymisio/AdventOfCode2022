57419fd1399f809aca4ac064013187a42680312c
Pendzoncymisio <pendzoncymisio@gmail.com>
2022-12-09 23:16:49 +0100

    Part1 buggy solution, but Agents boilerplate done

--- src/day9.jl
+++ src/day9.jl
@@ -7,18 +7,45 @@ input_array = readdlm(joinpath(@__DIR__,"..","inputs","day9.txt"),' ')
     type::Bool #true when head, false when tail
 end
 
-using Random # for reproducibility
-function part1(input_array)
-    space = GridSpace((100, 100), metric=:manhattan, periodic = false)
-    model = ABM(Knot, space)
+function initialize_model()
+    space = GridSpace((1000, 1000), metric=:chebyshev, periodic = false)
+    properties = Dict(:step => 1, :row => 1, :move => "R", :direction_dict => Dict("R"=>(1,0),"L"=>(-1,0),"U"=>(0,1),"D"=>(0,-1)), :head_prev_pos => (50,50), :head_history =>[], :tail_history => [])
+    model = ABM(Knot, space; properties = properties, scheduler = Schedulers.by_id)
 
-    head = Knot(1, (50, 50), true)
-    add_agent!(head, model)
+    head = Knot(1, (500, 500), true)
+    add_agent!(head, (500, 500), model)
 
-    tail = Knot(2, (50, 50), false)
-    add_agent!(head, model)
+    tail = Knot(2, (500, 500), false)
+    add_agent!(tail, (500, 500), model)
     
     return model
 end
 
-part1(1)
+function agent_step!(agent, model)
+    if agent.type #move head
+        model.head_prev_pos = agent.pos
+        walk!(agent,model.direction_dict[model.move],model)
+    else #move tail
+        if max(abs(model[1].pos[1] - agent.pos[1]),abs(model[1].pos[2] - agent.pos[2])) > 1
+            move_agent!(agent,model.head_prev_pos,model) 
+        end
+    end
+end
+
+function model_step!(model)
+    #println(model.step,model[1].pos,model[2].pos)
+    model.step += 1
+    push!(model.head_history,model[1].pos)
+    push!(model.tail_history,model[2].pos)
+end
+
+function part1(input_array, model)
+
+    for row in eachrow(input_array)
+        model.move = row[1]
+        step!(model,agent_step!,model_step!,row[2])
+    end
+    length(Set(model.tail_history))
+end
+
+part1(input_array, initialize_model())
