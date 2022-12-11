b3903a228f30d99a266180c8f63fb1c13108641d
Pendzoncymisio <pendzoncymisio@gmail.com>
2022-12-11 16:02:53 +0100

    Day10 part1 done

--- src/day10.jl
+++ src/day10.jl
@@ -6,9 +6,22 @@ input_array = readdlm(joinpath(@__DIR__,"..","inputs","day10.txt"),' ')
 function part1(input_array)
     cycle_count = 0
     X = 1
+    strenght_sum = 0
     for row in eachrow(input_array)
-        row[1] == "noop" && (cycle_count+=1)
+        if row[1] == "noop"
+            if (cycle_count+20) % 40 in [39]
+                strenght_sum += (cycle_count+1) * X
+            end
+            cycle_count+=1
+        else
+            if (cycle_count+20) % 40 in [38,39]
+                strenght_sum += (cycle_count + 40 - (cycle_count+20) % 40) *X
+            end
+            X += row[2]
+            cycle_count+=2
+        end
     end
+    strenght_sum
 end
 
 part1(input_array)
