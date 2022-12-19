f002ab275fbd516b206dd0b922ec4a1529b02a5f
Pendzoncymisio <pendzoncymisio@gmail.com>
2022-12-19 13:34:48 -0500

    Day18 part1 done

---
+++ src/day18.jl
@@ -0,0 +1,31 @@
+using DelimitedFiles
+using Pipe
+
+input_array = @pipe readdlm(joinpath(@__DIR__,"..","inputs","day18.txt"),' ',String) .|> split(_,',') .|> parse.(Int,_)
+
+function part1(input_array)
+    sides = 0
+
+    offsets = [
+        [1,0,0],
+        [-1,0,0],
+        [0,1,0],
+        [0,-1,0],
+        [0,0,1],
+        [0,0,-1]
+    ]
+
+    input_length = length(input_array)
+
+    for i in 1:input_length
+        element = pop!(input_array)
+        sides += 6
+        for offset in offsets
+            if (offset + element) in input_array
+                sides -= 2
+            end
+        end
+    end   
+    sides
+end
+part1(input_array)
